4a95e72f41f1a1048349f61d962b85fdfd3577e3
Rémi Meslin <remi.meslin@guarani.fr>
2023-10-11 11:59:49 +0200

    feature: quest

--- src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -18,7 +18,27 @@ public class SchoolRepository {
     private final static String DB_PASSWORD = "Horcrux4life!";
 
     public void deleteById(Long id) {
-        // TODO: delete a school from the database
+        Connection connection = null;
+        PreparedStatement statement = null;
+
+        try {
+            connection = DriverManager.getConnection(
+                    DB_URL, DB_USER, DB_PASSWORD
+            );
+            statement = connection.prepareStatement(
+                    "DELETE FROM school WHERE id=?;"
+            );
+            statement.setLong(1, id);
+
+            if (statement.executeUpdate() != 1) {
+                throw new SQLException("Failed to delete data");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JdbcUtils.closeConnection(connection);
+            JdbcUtils.closeStatement(statement);
+        }
     }
 
     public List<School> findAll() {
